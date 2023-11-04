e9a1f3cf2901e678554494026be5c919b3ab273e
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-04 00:44:27 -0300

    fix: correct error on header load

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+"use client"
 import { MenuIcon, SearchIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
@@ -12,9 +11,16 @@ import {
 } from "./sheet";
 import { Separator } from "./separator";
 import Link from "next/link";
+import { useState, useEffect } from 'react'
 
 export default function Header() {
-  return (
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  return isClient ?  (
     <Card className="flex items-center justify-between px-5 py-2">
       <Sheet>
         <SheetTrigger>
@@ -53,5 +59,5 @@ export default function Header() {
         </SheetContent>
       </Sheet>
     </Card>
-  );
+  ) : null;
 }
