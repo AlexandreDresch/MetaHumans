de79f9d28cde8f13e6ea9e7c536d9619de0c7367
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-04 00:42:01 -0300

    feat: add heroes context to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/ui/footer";
 import Header from "@/components/ui/header";
+import HeroesProvider from "./providers/heroes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex min-h-screen flex-col font-Poppins">
-          <Header />
-          <div className="flex-1">{children}</div>
-          <Footer />
+          <HeroesProvider>
+            <Header />
+            <div className="flex-1">{children}</div>
+            <Footer />
+          </HeroesProvider>
         </div>
       </body>
     </html>
