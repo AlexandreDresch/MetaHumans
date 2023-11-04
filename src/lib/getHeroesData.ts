5247a2310eb13677ca8707869bfe039d16eb2036
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-04 00:40:55 -0300

    feat: add request to the api

---
+++ src/lib/getHeroesData.ts
@@ -0,0 +1,13 @@
+export async function getHeroesData() {
+  const res = await fetch(
+    "http://homologacao3.azapfy.com.br/api/ps/metahumans"
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+
